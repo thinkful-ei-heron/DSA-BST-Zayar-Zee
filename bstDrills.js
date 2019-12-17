13f6ff01e3e69c7121ac111eb2c8a71a000c6193
zkhin <arandomname@gmail.com>
2019-12-17 16:44:49 -0500

    updates

--- bstDrills.js
+++ bstDrills.js
@@ -4,6 +4,7 @@ let bst = new BST()
 let bst2 = new BST()
 
 function tree(t, ) {
+  //Traverses the tree vertically then outputs the node value and then the values of the child nodes
   if (!t) {
     return 0;
   }
@@ -11,8 +12,63 @@ function tree(t, ) {
   return tree(t.left) + t.value + tree(t.right)
 }
 
+let count = 0
+
+function thirdLargest(t) {
+  if (!t) {
+
+  }
+
+  if (t.right) {
+      thirdLargest(t.right)
+  }
+
+  count += 1
+  if (count === 3) {
+    console.log('result: ', t.key)
+    return
+  }
+
+  if (t.left) {
+    thirdLargest(t.left)
+  }
+}
+
+function treeHeight(t, depth = 0) {
+  //runtime O(n)
+  if (!t) {
+    return depth;
+  }
+  if (t) {
+    depth += 1
+    return Math.max(
+      treeHeight(t.left, depth),
+      treeHeight(t.right, depth)
+    )
+  }
+
+
+}
+
+function isBST(t) {
+  if (!t) {
+    return true
+  }
+
+  if (t.left && t.left.key > t.key || t.right && t.right.key < t.key) {
+    return false
+  }
+
+  if (t) {
+    return (isBST(t.left) && isBST(t.right))
+  }
+  return true
+
+}
+
 function main() {
-  let input1 = [3, 1, 4, 6, 9, 2, 5, 7]
+  // let input1 = [3, 1, 4, 6, 9, 2, 5, 7, 8, 15]
+  let input1 = [3,2,4,1,6,10,7]
   let input2 = 'EASYQUESTION'.split('')
 
   input1.forEach((num) => {
@@ -23,8 +79,17 @@ function main() {
     bst2.insert(char)
   })
 
-  tree(bst)
-  tree(bst2)
+  const notBinary = new BST()
+  notBinary.key = 3
+  notBinary.left = new BST(key = 4, value = null, parent = notBinary)
+  notBinary.right = new BST(key = 2, value = null, parent = notBinary)
+
+  thirdLargest(bst)
+  // tree(bst2)
+  // console.log(treeHeight(bst))
+  // console.log(isBST(bst))
+  // console.log(isBST(notBinary))
+
 }
 
 main()
